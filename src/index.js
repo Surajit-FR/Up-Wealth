9aba64e501dbe2c12f14c5cfa73e936358040418
chandan-fr <georgesteve31019@gmail.com>
2024-03-21 11:53:22 +0530

    routes and folder setup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Router>
+    <Routes>
+      <Route path='*' element={<App />} />
+    </Routes>
+  </Router>
 );
 reportWebVitals();
